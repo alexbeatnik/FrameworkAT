c5265b9a19a3a26dc757f1bdad3a564cffddf73b
alexbeatnik <alexbeatnik@gmail.com>
2022-02-01 18:00:22 +0200

    add webdriver, geckodriver and simple test

--- ATesting/UnitTest1.cs
+++ ATesting/UnitTest1.cs
@@ -1,9 +1,14 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace ATesting
 {
     public class Tests
     {
+        private IWebDriver _driver;
+        string url = "http://eaapp.somee.com/";
+
         [SetUp]
         public void Setup()
         {
@@ -12,7 +17,19 @@ namespace ATesting
         [Test]
         public void Test1()
         {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(url);
+            LogIn();
             Assert.Pass();
+        }
+
+        public void LogIn()
+        {
+            _driver.FindElement(By.LinkText("Login")).Click();
+            _driver.FindElement(By.Id("UserName")).SendKeys("admin");
+            _driver.FindElement(By.Id("Password")).SendKeys("password");
+            _driver.FindElement(By.CssSelector("input.btn")).Submit();
+
         }
     }
 }
