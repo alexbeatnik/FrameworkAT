69282da15a55ee55f8ba0eac0c8500ee4d3afd0d
alexbeatnik <alexbeatnik@gmail.com>
2022-02-04 17:42:08 +0200

    Add Dropdown element and Assert element present

--- ATFramework/Extentions/WebElementExentions.cs
+++ ATFramework/Extentions/WebElementExentions.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace ATFramework.Extentions
 {
-    internal class WebElementExentions
+    public static class WebElementExentions
     {
+
+        public static void SelectDropDownList(this IWebElement element, string value)
+        {
+            SelectElement ddl = new SelectElement(element);
+        }
+
+        public static void AssertElementPresent(this IWebElement element)
+        {
+            if (!IsElementPresent(element))
+            {
+                throw new Exception(string.Format("Element Not Present Exception")); 
+            }
+        }
+
+        private static bool IsElementPresent(IWebElement element)
+        {
+            try
+            {
+                bool ele = element.Displayed;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
