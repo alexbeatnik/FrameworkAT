d33c32472dcd27a4076ca573ef85391bbb45f2c5
alexbeatnik <alexbeatnik@gmail.com>
2022-02-05 17:34:44 +0200

    Add wait for page ready

--- ATFramework/Extentions/WebDriverExtentions.cs
+++ ATFramework/Extentions/WebDriverExtentions.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using System;
+using System.Diagnostics;
 
 namespace ATFramework.Extentions
 {
-    internal class WebDriverExtentions
+    public static class WebDriverExtentions
     {
+        public static void WaitForPageLoaded(this IWebDriver driver)
+        {
+            driver.WaitForCondition(dri =>
+            {
+                string state = dri.ExecuteJs("return document.readyState").ToString();
+                return state == "complete";
+            }, 10);
+        }
+
+        public static void WaitForCondition<T>(this T obj, Func<T, bool> comdition, int timeOut)
+        {
+            Func<T, bool> execute = (arg) =>
+            {
+                try
+                {
+                    return comdition(arg);
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            };
+            var stopWatch = Stopwatch.StartNew();
+            while(stopWatch.ElapsedMilliseconds < timeOut)
+            {
+                if (execute(obj))
+                {
+                    break;
+                }
+            }
+        }
+
+        internal static object ExecuteJs(this IWebDriver driver, string script)
+        {
+            return driver.ExecuteJs(script);
+        }
     }
 }
